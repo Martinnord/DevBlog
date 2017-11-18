a2240bcfb4ae2b11f3733ce0468ed5edf7d36437
martinnord <martin.nordstrom99@gmail.com>
2017-11-18 04:35:25 +0100

    added an index.js file in my models file so I just can import my models into it and doesn't have to bother import it in other files

--- backend/index.js
+++ backend/index.js
@@ -1,58 +1,54 @@
-import express from 'express'
-import middlewares from './config/middlewares'
-import constants from './config/constants'
-const config = require('./config')
-const routes = require('./routes')
-const { 
-  graphqlExpress,
-  graphiqlExpress
-} = require('apollo-server-express')
-const { makeExecutableSchema } = require('graphql-tools')
-const { createServer } = require('http')
-import typeDefs from './graphql/schema'
-const resolvers = require('./graphql/resolvers')
+import express from "express";
+import middlewares from "./config/middlewares";
+import constants from "./config/constants";
+const config = require("./config");
+const routes = require("./routes");
+const { graphqlExpress, graphiqlExpress } = require("apollo-server-express");
+const { makeExecutableSchema } = require("graphql-tools");
+const { createServer } = require("http");
+import typeDefs from "./graphql/schema";
+const resolvers = require("./graphql/resolvers");
 // const knex = require('./config/database')
-const app = express()
-const schema = makeExecutableSchema({ typeDefs, resolvers })
+const app = express();
+const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-import Knex from 'knex';
-import { Model } from 'objection';
-import knexConfig from './knexfile';
-import Post from './models/post.model';
-import User from './models/user.model';
+import Knex from "knex";
+import { Model } from "objection";
+import knexConfig from "./knexfile";
+import { Post, User } from "./models";
 
 const knex = Knex(knexConfig.development);
 Model.knex(knex);
 
-middlewares(app)
+middlewares(app);
 
 app.use(
-  '/graphql',
+  "/graphql",
   graphiqlExpress({
-    endpointURL: config.GRAPHQL_PATH,
+    endpointURL: config.GRAPHQL_PATH
   })
-)
+);
 
 app.use(
   constants.GRAPHQL_PATH,
   graphqlExpress({
     schema
   })
-)
+);
 
-app.use('/api', routes)
+app.use("/api", routes);
 
-const graphQLServer = createServer(app)
+const graphQLServer = createServer(app);
 
 graphQLServer.listen(3010, err => {
   if (err) {
-    console.log(`Error: ${err}`)
+    console.log(`Error: ${err}`);
   } else {
     console.log(`
       App listening on 3001
       Env: ${process.env.NODE_ENV}
-    `)
+    `);
   }
-})
+});
 
-export default app
+export default app;
---
+++ backend/models/index.js
@@ -0,0 +1,7 @@
+import User from "./user.model";
+import Post from "./post.model";
+
+module.exports = {
+  User,
+  Post
+};
