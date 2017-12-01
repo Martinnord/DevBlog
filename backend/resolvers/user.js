b2cefda27918055b4d372f88b3c6bb749a1c20e9
martinnord <martin.nordstrom99@gmail.com>
2017-12-01 14:07:53 +0100

    format

--- backend/resolvers/user.js
+++ backend/resolvers/user.js
@@ -1,7 +1,7 @@
 import GraphQLDate from "graphql-date";
 import User from "../models/user";
 import bcrypt from "bcryptjs";
-const { promisify } = require("util");
+import { promisify } from "util";
 
 const hashSync = promisify(bcrypt.hash);
 
@@ -18,18 +18,8 @@ export default {
   Mutation: {
     async createUser(_, { email, username, password }) {
       const hash = await hashSync(password, bcrypt.genSaltSync(10));
-			console.log(hash)
-      return await User.query().insert({ email, username, password: hash });
+      return User.query().insert({ email, username, password: hash });
     }
-    // async createUser(_, { email, username, password }) {
-    //   const passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-    //     return await User.query().insert({ email, username, password: passwordHash });
-    // }
   }
 };
 
-//     const salt = await bcrypt.genSalt(10)
-//     // Generate a password hash (salt + hash)
-//     const passwordHash = await bcrypt.hash(user.local.password, salt)
-//     // Re-assign hashed version over original, plain text password
-//     user.local.password = passwordHash
