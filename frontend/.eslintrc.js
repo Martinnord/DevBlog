ad84e48433990960d527690ab5faf3b0c8fd7b60
martinnord <martin.nordstrom99@gmail.com>
2018-01-04 07:56:18 +0100

    added new rules for eslint config

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   extends: 'airbnb',
-  plugins: ['react', 'jsx-a11y', 'import']
+  plugins: ['react', 'jsx-a11y', 'import'],
+  "rules": {
+    "react/jsx-filename-extension": 0,
+    "react/prefer-stateless-function": 0,
+    "react/prop-types": 0,
+    "react/no-unescaped-entities": 0,
+    semi: [2, "never"]
+  }
 }
