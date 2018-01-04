456f85c39608af1e476b94dd73c07416e2e2f860
martinnord <martin.nordstrom99@gmail.com>
2018-01-04 08:31:32 +0100

    removed Redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,15 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Provider } from 'react-redux'
 import { ApolloProvider } from 'react-apollo'
 import 'antd/dist/antd.css'
 import client from './client'
 import Router from './routes/router'
-import store from './store'
 import './styles/css/index.css'
 
 
 const App = (
   <ApolloProvider client={client}>
-    <Provider store={store}>
-      <Router />
-    </Provider>
+    <Router />
   </ApolloProvider>
 )
 
--- frontend/src/routes/router.js
+++ frontend/src/routes/router.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import PostLayout from '../components/posts/postsLayout'
-import CreatePostLayout from '../components/createPost/createPostLayout'
 import Home from '../components/home/homeLayout'
 import Register from './Register'
 import Login from './Login'
@@ -9,9 +7,7 @@ import Login from './Login'
 export default props => (
   <Router>
     <div>
-      <Route exact path="/" component={PostLayout} />
       <Route exact path="/home" component={Home} />
-      <Route exact path="/agerallt" component={CreatePostLayout} />
       <Route exact path="/register" component={Register} />
       <Route exact path="/login" component={Login} />
     </div>
