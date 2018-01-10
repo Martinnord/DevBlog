40a203ba74b662c3430301755fed9e39333e2bc5
martinnord <martin.nordstrom99@gmail.com>
2018-01-10 14:04:04 +0100

    renamed the created_at to createdAt. The same goes for the other two functions

--- backend/models/post.js
+++ backend/models/post.js
@@ -13,20 +13,20 @@ export default class Post extends Model {
       properties: {
         id: { type: 'integer' },
         title: { type: 'string', minLength: 1, maxLength: 255 },
-        content: { type: 'string', minLength: 1, maxLength: 255 }
+        content: { type: 'string', minLength: 1, maxLength: 255 },
       }
     }
   }
 
   $beforeInsert() {
-    this.created_at = new Date().toISOString()
+    this.createdAt = new Date().toISOString()
   }
 
   $beforeUpdate() {
-    this.updated_at = new Date().toISOString()
+    this.updatedAt = new Date().toISOString()
   }
 
   $beforeDelete() {
-    this.deleted_at = new Date().toISOString()
+    this.deletedAt = new Date().toISOString()
   }
 }
