1e0d667336141fbd5af367c1dd4dc059d4b4b8ad
martinnord <martin.nordstrom99@gmail.com>
2018-01-10 06:50:08 +0100

    tried both functional and classes based components when using the apollo client

--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -1,9 +1,38 @@
 import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
-import React from 'react'
+import React, { Component } from 'react'
+import { Layout, Menu } from 'antd'
+const { Header, Content } = Layout
 
-const Home = ({ data: { getPosts = [] } }) =>
-  getPosts.map(u => <h1 key={u.id}>{u.title}</h1>)
+const Home = ({ data: { getPosts = [] } }) => {
+  return (
+    <ul>
+      {getPosts.map(u => <h1 key={u.id}>{u.title}</h1>)}
+    </ul>
+  )
+}
+
+// class Home extends Component {
+//   render() {
+//     const allPosts = (
+//       this.props.data.getPosts.map(u => <h1>{u.title}</h1>)
+//     )
+
+//     return (
+//       <Layout>
+//         <Header>
+//           <Menu
+//             theme="dark"
+//             mode="horizontal"
+//           >
+//             <Menu.Item>DEVBLOG</Menu.Item>
+//           </Menu>
+//         </Header>
+//         <Content>{allPosts}</Content>
+//       </Layout>
+//     )
+//   }
+// }
 
 const getPostsQuery = gql`
   query getPostsQuery {
