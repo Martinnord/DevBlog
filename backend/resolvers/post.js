80ca905653a03189c382b54d2cb29163e11e6b48
martinnord <martin.nordstrom99@gmail.com>
2018-01-19 17:49:04 +0100

    Getting the user by it's ID so I can get the users values

--- backend/resolvers/post.js
+++ backend/resolvers/post.js
@@ -1,10 +1,15 @@
 import GraphQLDate from 'graphql-date'
 import Post from '../models/post'
 import yup from 'yup'
-
+import User from '../models/user'
 
 export default {
   Date: GraphQLDate,
+  Post: {
+    user: ({ user_id }) => {
+      return User.query().findById(user_id)
+    }
+  },
   Query: {
     getAllPosts: async () => {
       return await Post.query().orderBy('createdAt', 'desc')
@@ -14,14 +19,9 @@ export default {
     }
   },
   Mutation: {
-    createPost: async (_, { title, content }) => {
-      try {
-        const post = await Post.query().insert({ title, content })
-        return post
-      } catch (err) {
-        console.log(err)
-        return false
-      }
+    createPost: async (_, { title, content }, { user }) => {
+      const userId = user.id
+      return await Post.query().insert({ title, content, user_id: userId })
     },
     updatePost: async (_, { id, title, content }) => {
       return await Post.query().patchAndFetchById(id, { title, content })
