807c91e7e48cd5e3956cd71d74ecf01122c43b79
martinnord <martin.nordstrom99@gmail.com>
2018-03-09 14:36:37 +0100

    updated knexfile

--- backend/src/knexfile.js
+++ backend/src/knexfile.js
@@ -13,9 +13,11 @@ module.exports = {
     client: 'pg',
     // connection: `postgres://${process.env.DB_HOST}/devblog`,
     connection: {
+      host: process.env.DB_HOST,
       database: process.env.PROD_DATABASE,
       user: process.env.PROD_USER,
-      password: process.env.PROD_PASSWORD
+      password: process.env.PROD_PASSWORD,
+      port: process.env.PROD_PORT,
     },
     migrations: {
       directory: __dirname + '/config/migrations'
