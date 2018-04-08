6d97d15bcce748cfc3d4ac6a1859c9bd31e449c6
martinnord <martin.nordstrom99@gmail.com>
2018-04-08 05:01:46 +0200

    minor changes

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -2,15 +2,16 @@ import React from 'react'
 import moment from 'moment'
 import { Link } from 'react-router-dom'
 import { Icon, Col } from 'antd'
+import { EditorState, RichUtils, convertFromRaw } from 'draft-js'
 
 import '../routes/index.css'
 
 const Post = ({ post }) => (
   <Col span={12} offset={6}>
-    <img className="post-image" src={post.imageUrl} alt="post_image" />
-    <h1 className="post-title">{post.title}</h1>
+    {/* <img className="post-image" src={post.imageUrl} alt="post_image" /> */}
+    <h1 className="post-title">{post}</h1>
     <div style={{ display: 'flex' }}>
-      <h3>Written by </h3>
+      {/* <h3>Written by </h3>
       <Link to={`/@${post.user.username}`}>
         {post.user.name || post.user.username}
       </Link>
@@ -19,10 +20,10 @@ const Post = ({ post }) => (
       </Link>
       <Link to={`https://github.com/${post.user.githubUsername}`}>
         <Icon type="github" />
-      </Link>
+      </Link> */}
     </div>
     <h3>{moment(post.createdAt).format('MMM. D[,] YYYY')}</h3>
-    <p className="post-content">{post.content}</p>
+    {/* <p className="post-content">{convertFromRaw(JSON.parse(post.content))}</p> */}
   </Col>
 )
 
