74b4390c22212676d0b35474a2475d742e6de1bc
martinnord <martin.nordstrom99@gmail.com>
2018-04-15 13:30:28 +0200

    going to remove the description field

--- frontend/src/components/Postcard.js
+++ frontend/src/components/Postcard.js
@@ -2,32 +2,48 @@ import React from 'react'
 import { Card, Col } from 'antd'
 import moment from 'moment'
 import { Link } from 'react-router-dom'
+import { Value, Text } from 'slate'
+import Plain from 'slate-plain-serializer'
+import { Editor } from 'slate-react'
+
 import '../routes/index.css'
 
 const { Meta } = Card
 
-const PostCard = props => (
+const PostCard = ({ posts }) => (
   <Col span={12} offset={6}>
-    {props.posts.map(post => (
-      <Link to={`/@${post.user.username}/${post.id}`} key={post.id}>
-        <Card
-          style={{ margin: '20px 0 20px 0' }}
-          hoverable
-          cover={<img src={`${post.imageUrl ? `${post.imageUrl}` : ''}`} />}
-        >
-          <Meta
-            title={post.title}
-            description={`${
-              post.content.length > 135
-                ? `${post.content.substring(0, 135)}...`
-                : post.content
-            }`}
-          />
-          <p>Written by {post.user.username}</p>
-          <p>{moment(post.createdAt).format('HH:mm D/MM')}</p>
-        </Card>
-      </Link>
-    ))}
+    {posts.map(post => {
+      const content = Value.fromJSON(JSON.parse(post.content))
+      const lol = JSON.stringify(content)
+      return (
+        <Link to={`/@${post.user.username}/${post.id}`} key={post.id}>
+          <Card
+            style={{ margin: '20px 0 20px 0' }}
+            hoverable
+            cover={<img src={`${post.imageUrl ? `${post.imageUrl}` : ''}`} />}
+          >
+            <Meta
+              title={post.title}
+              description={
+                <Editor
+                  readOnly
+                  value={content}
+                />
+              }
+              // description={content}
+              // description={`${
+              //   post.content.length > 135
+              //     ? `${post.content.substring(0, 135)}...`
+              //     : post.content
+              // }`}
+            />
+            <p>Written by {post.user.username}</p>
+            {/* <p>{Value.fromJSON(post.content)}</p> */}
+            <p>{moment(post.createdAt).format('HH:mm D/MM')}</p>
+          </Card>
+        </Link>
+      )
+    })}
   </Col>
 )
 
