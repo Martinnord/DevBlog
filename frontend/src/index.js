f4df5d4180aab66332b370a04e0b53635bbfb81a
martinnord <martin.nordstrom99@gmail.com>
2018-04-17 22:17:50 +0200

    Clean up

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,15 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { ApolloProvider, createNetworkInterface } from 'react-apollo'
+import { ApolloProvider } from 'react-apollo'
 import { createHttpLink } from 'apollo-link-http'
 import { setContext } from 'apollo-link-context'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloClient } from 'apollo-client'
-import { WebSocketLink } from 'apollo-link-ws'
 import { split } from 'apollo-link'
-import { getMainDefinition } from 'apollo-utilities'
-import {
-  SubscriptionClient,
-  addGraphQLSubscriptions
-} from 'subscriptions-transport-ws'
-import App from './routes'
-import constants from './config'
-
+import { SubscriptionClient } from 'subscriptions-transport-ws'
 import 'antd/dist/antd.css'
+import App from './routes'
+
 import './styles/css/index.css'
 
 const authLink = setContext((_, { headers }) => {
@@ -25,23 +19,22 @@ const authLink = setContext((_, { headers }) => {
     headers: {
       ...headers,
       authorization: token ? `Bearer ${token}` : null
-    }
+    },
   }
 })
 
 const httpLink = createHttpLink({ uri: 'http://localhost:3010/graphql' })
 
 const wsClient = new SubscriptionClient('ws://localhost:3010/subscriptions', {
-  reconnect: true
+  reconnect: true,
 })
 
 const httpLinkWithMiddleware = authLink.concat(httpLink)
 
-
 const link = split(
   ({ query }) => {
-    const { kind, operation } = getMainDefinition(query);
-    return kind === 'OperationDefinition' && operation === 'subscription';
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
   },
   wsClient,
   httpLinkWithMiddleware,
@@ -49,7 +42,7 @@ const link = split(
 
 const client = new ApolloClient({
   link,
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
 })
 
 const Application = (
--- frontend/src/routes/PostLayout.js
+++ frontend/src/routes/PostLayout.js
@@ -3,50 +3,45 @@ import { Redirect } from 'react-router-dom'
 import gql from 'graphql-tag'
 import { graphql, compose } from 'react-apollo'
 import { Layout } from 'antd'
+import { Value } from 'slate'
 import Navbar from '../common/Navbar'
 import Post from '../components/Post'
 import { getAllPostsQuery } from '../graphql/newArticle'
-import { Value } from 'slate'
 
 import './index.css'
 
 const { Content } = Layout
 
 const postLikedSubscription = gql`
-subscription ($id: ID!) {
-  postLiked(id: $id) {
-    id
-    likes {
-      username
+  subscription($id: ID!) {
+    postLiked(id: $id) {
+      id
+      likes {
+        username
+      }
     }
   }
-}
 `
-
 
 class PostLayout extends Component {
   componentWillMount() {
-    console.log(this.props)
     this.props.data.subscribeToMore({
       document: postLikedSubscription,
       variables: {
         id: this.props.data.variables.id,
       },
-      updateQuery: (prev, {subscriptionData}) => {
-        console.log('subscriptionData', subscriptionData)
-        console.log('prev', prev)
+      updateQuery: (prev, { subscriptionData }) => {
         if (!subscriptionData.data) {
-          return prev;
+          return prev
         }
 
         return {
           ...prev,
-          likes: [...prev.likes, subscriptionData.data.postLiked]
-        }    
-       }
-    });
+          likes: [...prev.likes, subscriptionData.data.postLiked],
+        }
+      },
+    })
   }
-  
 
   render() {
     const { loading, getPost } = this.props.data
@@ -114,42 +109,26 @@ export default compose(
   graphql(getPostQuery, {
     skip: props => !props.match.params.id,
     options: props => ({
-      variables: { id: props.match.params.id }
-    })
+      variables: { id: props.match.params.id },
+    }),
   }),
   graphql(likePostMutation, {
     alias: 'likePost',
     props: ({ mutate, ownProps }) => ({
       likePost: () => {
-        console.log('mutate', typeof ownProps.data.getPost.id)
         mutate({
           variables: {
-            id: ownProps.data.getPost.id
+            id: ownProps.data.getPost.id,
           },
           update: (store, { data: likePost }) => {
             const data = store.readQuery({
-              query: getAllPostsQuery
+              query: getAllPostsQuery,
             })
             data.getAllPostsQuery.push(likePost)
             store.writeQuery({ query: getAllPostsQuery, data })
-          }
+          },
         })
-      }
-    })
-  })
+      },
+    }),
+  }),
 )(PostLayout)
-
-// update: (proxy, { data: { register } }) => {
-//   // update the cache with the new currentUser
-//   const currentUser = {
-//     id: register.id,
-//     username: register.username,
-//     email,
-//     jwt: register.jwt,
-//   }
-
-//   proxy.writeQuery({ query: currentUserQUERY, data: { currentUser } })
-
-//   // Store the token
-//   localStorage.setItem('token', register.jwt)
-// },
