c7fd97b4e489b9ba1597c2487c5d741b1916dc02
martinnord <martin.nordstrom99@gmail.com>
2018-04-17 21:54:48 +0200

    app supports subscriptions

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { ApolloProvider } from 'react-apollo'
+import { ApolloProvider, createNetworkInterface } from 'react-apollo'
 import { createHttpLink } from 'apollo-link-http'
 import { setContext } from 'apollo-link-context'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloClient } from 'apollo-client'
+import { WebSocketLink } from 'apollo-link-ws'
+import { split } from 'apollo-link'
+import { getMainDefinition } from 'apollo-utilities'
+import {
+  SubscriptionClient,
+  addGraphQLSubscriptions
+} from 'subscriptions-transport-ws'
 import App from './routes'
 import constants from './config'
 
@@ -22,15 +29,26 @@ const authLink = setContext((_, { headers }) => {
   }
 })
 
-console.log('constants', constants)
+const httpLink = createHttpLink({ uri: 'http://localhost:3010/graphql' })
 
-const httpLink = createHttpLink({
-  // uri: 'http://devblogapi.vyralynn.com/graphql'
-  uri: 'http://localhost:3010/graphql'
+const wsClient = new SubscriptionClient('ws://localhost:3010/subscriptions', {
+  reconnect: true
 })
 
+const httpLinkWithMiddleware = authLink.concat(httpLink)
+
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsClient,
+  httpLinkWithMiddleware,
+)
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache()
 })
 
