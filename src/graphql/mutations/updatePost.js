9a25b19f5acfb7a4b686904bf6d9371a33034e67
martinnord <martin.nordstrom99@gmail.com>
2018-04-29 13:16:29 +0200

    just testing

--- src/graphql/mutations/updatePost.js
+++ src/graphql/mutations/updatePost.js
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag'
 
 export default gql`
-  mutation($id: Int!, $title: String!, $content: String!) {
-    updatePost(id: $id, title: $title, content: $content) {
+  mutation($id: Int!, $title: String!, $content: String! $image_url: String) {
+    updatePost(id: $id, title: $title, content: $content, image_url: $image_url) {
       id
       title
       content
