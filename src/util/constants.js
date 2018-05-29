c0cfae1ed8791d60ae75a395178c27579884e14c
martinnord <martin.nordstrom99@gmail.com>
2018-05-29 13:21:22 +0200

    added new file

---
+++ src/util/constants.js
@@ -0,0 +1,3 @@
+export const REACT_APP_GRAPHQL_URL = 'http://localhost:3010/graphql'
+export const REACT_APP_GRAPHQL_SUBSCRIPTIONS_URL = 'ws://localhost:3010/subscriptions'
+export const NODE_ENV = 'development'
